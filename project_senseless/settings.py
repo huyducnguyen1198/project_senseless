dab7939d616cf08136045fae93aa8a5c472b4ac4
Huy Nguyen <lordndhuy@gmail.com>
2024-01-26 07:42:11 -0700

    add mysql on azure cloud

--- project_senseless/settings.py
+++ project_senseless/settings.py
@@ -82,12 +82,13 @@ WSGI_APPLICATION = "project_senseless.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': os.getenv('DATABASE_ENGINE'),
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv("DATABASE_PASSWORD"),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),
+
     }
 }
 
